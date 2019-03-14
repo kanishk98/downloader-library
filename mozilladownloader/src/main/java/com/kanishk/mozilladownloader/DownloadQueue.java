cd3680a665ed7995157fdda7f9ae101cf64c9457
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-03-14 10:25:08 +0530

    Replace QueueFile with PriorityQueue

---
+++ mozilladownloader/src/main/java/com/kanishk/mozilladownloader/DownloadQueue.java
@@ -0,0 +1,7 @@
+package com.kanishk.mozilladownloader;
+
+import java.util.PriorityQueue;
+
+public class DownloadQueue {
+    private PriorityQueue<MozillaDownload> queue;
+}
--- mozilladownloader/src/main/java/com/kanishk/mozilladownloader/MozillaDownloader.java
+++ mozilladownloader/src/main/java/com/kanishk/mozilladownloader/MozillaDownloader.java
@@ -3,16 +3,19 @@ package com.kanishk.mozilladownloader;
 import android.content.Context;
 
 import com.google.gson.Gson;
-import com.squareup.tape2.QueueFile;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class MozillaDownloader {
     
     private static final MozillaDownloader downloader = new MozillaDownloader();
-    private QueueFile queueFile;
-    private static final String TAG = "MozillaDownloader";
+    private Context context;
+    private PriorityQueue<MozillaDownload> downloadQueue;
     private DownloadStatusListener downloadStatusListener;
     
     private MozillaDownloader() {
@@ -23,22 +26,46 @@ public class MozillaDownloader {
         this.downloadStatusListener = downloadStatusListener;
     }
 
-    public QueueFile initDownloader(Context context) throws IOException {
-        // must call initDownloader() before using any other function from this class
+    private boolean flushQueueToDisk(PriorityQueue<MozillaDownload> queue) {
         File file = new File(context.getFilesDir() + "queue_file.json");
-        if (!file.exists()) {
-            queueFile = new QueueFile.Builder(file).build();
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(new Gson().toJson(queue));
+            bufferedWriter.close();
+            return true;
+        } catch (IOException ioException) {
+            return false;
         }
-        return queueFile;
     }
 
+    // TODO: What if app accidentally calls initDownloader() with a different context?
+    public void initDownloader(Context context) throws IOException {
+        // must call initDownloader() before using any other function from this class
+        this.context = context;
+        downloadQueue = new PriorityQueue<>(0, new Comparator<MozillaDownload>() {
+            @Override
+            public int compare(MozillaDownload a, MozillaDownload b) {
+                return a.getUid().compareTo(b.getUid());
+            }
+        });
+    }
 
-    public void scheduleDownload(MozillaDownload download) throws IOException {
-        // add download to queueFile
+    public void scheduleDownload(MozillaDownload download) {
         download.setStatus(DownloadStatus.SCHEDULED);
         downloadStatusListener.onStatusChange(download);
-        String downloadJson = new Gson().toJson(download);
-        queueFile.add(downloadJson.getBytes());
+        downloadQueue.add(download);
+    }
+
+    public boolean pauseDownload(MozillaDownload download) {
+        return false;
+    }
+
+    public boolean cancelDownload(MozillaDownload download) {
+        return false;
+    }
+
+    public boolean rescheduleDownload(MozillaDownload download) {
+        return false;
     }
 
     private static MozillaDownloader getDownloader() {
