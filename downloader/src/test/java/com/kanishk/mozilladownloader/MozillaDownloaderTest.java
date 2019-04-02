fc420d70c7ae0bb481bbc22be55ffdc360cd94e1
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-04-02 05:51:08 +0530

    Add test for checking if created intent is valid

--- downloader/src/test/java/com/kanishk/mozilladownloader/MozillaDownloaderTest.java
+++ downloader/src/test/java/com/kanishk/mozilladownloader/MozillaDownloaderTest.java
@@ -1,15 +1,20 @@
 package com.kanishk.mozilladownloader;
 
 import android.content.Context;
+import android.content.Intent;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
 
 import java.util.Calendar;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(RobolectricTestRunner.class)
 public class MozillaDownloaderTest {
 
     private MozillaDownloader downloader;
@@ -54,5 +59,17 @@ public class MozillaDownloaderTest {
                 .setUrl("https://www.github.com")
                 .setScheduledTime(null);
         download = builder.createMozillaDownload();
+    }
+
+    @Test
+    public void isCreatedIntentValid() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2000, 0, 0);
+        MozillaDownloadBuilder builder = new MozillaDownloadBuilder()
+                .setUrl("https://www.github.com")
+                .setScheduledTime(calendar.getTime());
+        download = builder.createMozillaDownload();
+        Intent intent = MozillaDownloader.createIntent(context, download);
+        assertEquals(download, (MozillaDownload) intent.getSerializableExtra("MozillaDownload"));
     }
 }
